ef279a6e1e3a2475955eacec402539c64ef2b997
Gary A. Stafford <garystafford@rochester.rr.com>
2019-07-23 07:36:07 -0400

    Refactor scripts

---
+++ parameter_store_values.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# Change me!
+KEY_PATH="file:///Users/garystafford/.ssh/ansible"
+PARAMETER_PATH="/ansible_demo"
+
+# Put parameters
+aws ssm put-parameter \
+  --name $PARAMETER_PATH/ansible_private_key \
+  --type SecureString \
+  --value $KEY_PATH \
+  --description "Private key for EC2 instances" \
+  --overwrite
+
+aws ssm put-parameter \
+  --name $PARAMETER_PATH/ssh_location \
+  --type String \
+  --value "0.0.0.0/0" \
+  --description "IP Address Range from which SSH is allowed" \
+  --overwrite
+
+aws ssm put-parameter \
+  --name $PARAMETER_PATH/instance_type \
+  --type String \
+  --value "t2.small" \
+  --description "Instance type of Web Servers" \
+  --overwrite
+
+aws ssm put-parameter \
+  --name $PARAMETER_PATH/key_name \
+  --type String \
+  --value "ansible" \
+  --description "SSH key name" \
+  --overwrite
+
+aws ssm put-parameter \
+  --name $PARAMETER_PATH/vpc_cidr \
+  --type String \
+  --value "10.0.0.0/24" \
+  --description "VPC CIDR" \
+  --overwrite
+
+aws ssm put-parameter \
+  --name $PARAMETER_PATH/public_subnet_1_cidr \
+  --type String \
+  --value "10.0.0.0/28" \
+  --description "Public subnet in the first AZ" \
+  --overwrite
+
+aws ssm put-parameter \
+  --name $PARAMETER_PATH/public_subnet_2_cidr \
+  --type String \
+  --value "10.0.0.16/28" \
+  --description "Public subnet in the second AZ" \
+  --overwrite
+
+aws ssm put-parameter \
+  --name $PARAMETER_PATH/private_subnet_1_cidr \
+  --type String \
+  --value "10.0.0.32/28" \
+  --description "Private subnet in the first AZ" \
+  --overwrite
+
+aws ssm put-parameter \
+  --name $PARAMETER_PATH/private_subnet_2_cidr \
+  --type String \
+  --value "10.0.0.48/28" \
+  --description "Private subnet in the second AZ" \
+  --overwrite
+
+# Spot check parameters
+aws ssm get-parameter \
+  --with-decryption \
+  --name $PARAMETER_PATH/ansible_private_key \
+  --query Parameter.Value
+
+aws ssm get-parameter \
+  --name $PARAMETER_PATH/instance_type \
+  --query Parameter.Value
